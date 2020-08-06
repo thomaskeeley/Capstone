0feb284f579f863865b1a7c4751926f7c5702cc3
thomaskeeley <40431739+thomaskeeley@users.noreply.github.com>
2020-08-05 21:14:12 -0400

    Update main.py

--- Thomas/main.py
+++ Thomas/main.py
@@ -20,27 +20,26 @@ from object_detection import ObjectDetection
 if __name__ == '__main__':
     
     instance = ObjectDetection(
-    working_dir = '',
-    import_image_path = None,
-    download_image = True,
-    aoi_path = 'test_aoi_small.geojson',
-    username = '',
-    password = '',
+    working_dir = '...',
+    import_image_path = '.../nk_aoi.tif',
+    download_image = False,
+    aoi_path = None,
+    username = 'sirthomasnewton',
+    password = 'Coronavirus1!',
     date_range = ('20190101', '20200101'),
-    mask_path = None,
-    out_dir = '',
+    mask_path = '.../nk_aoi_b.geojson',
+    out_dir = '...',
     step = 4,
     prediction_threshold = 0.90,
-    pos_chip_dir = ('/positive_chips/'),
-    neg_chip_dir = ('/negative_chips/'),
+    pos_chip_dir = ('.../positive_chips/'),
+    neg_chip_dir = ('.../negative_chips/'),
     chip_width = 20,
     chip_height = 20,
     augment_pos_chips = True,
     augment_neg_chips = False,
-    save_model = True,
+    save_model = False,
     import_model = None,
-    segmentation = True)
-    
+    segmentation = False)
     
     instance.execute()
     
