afaebc7aa05a8014c46c5869ce435d9d3c026c1b
thomaskeeley <40431739+thomaskeeley@users.noreply.github.com>
2020-08-13 16:32:29 -0400

    Update get_sentinel_imagery.py

--- Thomas/get_sentinel_imagery.py
+++ Thomas/get_sentinel_imagery.py
@@ -22,7 +22,27 @@ import matplotlib.pyplot as plt
 class GetSentinelImagery:
     
     def __init__(self, image_dir, aoi_dir, username, password, date_range):
+        """
         
+
+        Parameters
+        ----------
+        image_dir : Directory
+            Desired output path of downloaded imagery
+        aoi_dir : Directory
+            Filepath to AOI file to be used in querying data
+        username : String
+            Copernicus Hub username
+        password : String
+            Copernicus Hub password
+        date_range : Tuple
+            Desired start and end date for querying data
+
+        Returns
+        -------
+        None.
+
+        """
         self.image_dir = image_dir
         self.aoi_dir = aoi_dir
         self.username = username
@@ -30,6 +50,15 @@ class GetSentinelImagery:
         self.date_range = date_range
         
     def get_image_products(self):
+        """
+        
+
+        Returns
+        -------
+        products : List
+            Contains collection of information of available images from query
+
+        """
         print('\n >>> CONDUCTING API QUERY')
         api = SentinelAPI(self.username, self.password, 'https://scihub.copernicus.eu/dhus')
         shape = gpd.read_file(self.aoi_dir)
@@ -53,6 +82,19 @@ class GetSentinelImagery:
     
     
     def download_image(self):
+        """
+        
+
+        Returns
+        -------
+        image_path : Directory
+            Filepath to downloaded image after unzipping
+        date : String
+            Date of downloaded image
+        time : String
+            Zulu time of downloaded image
+
+        """
         api = SentinelAPI(self.username, self.password, 'https://scihub.copernicus.eu/dhus')
         products = self.get_image_products()
         key = products.index[0]
@@ -70,80 +112,3 @@ class GetSentinelImagery:
         os.remove(title + '.zip')           
         return image_path, date, time
     
-
-    # def clip_image(self):
-    #     image_path = self.download_image()
-    #     image = rasterio.open(image_path)
-    #     crs = str(image.crs).split(':')[1] 
-    #     shape = gpd.read_file(self.aoi_dir)
-    #     shape = shape.to_crs({'init': 'epsg:{}'.format(crs)})
-    #     print('\n >>> CROPPING IMAGE')
-    #     with fiona.open(self.aoi_dir, "r") as shape:
-    #             geoms = [feature["geometry"] for feature in shape]
-            
-    #             clipped_image, out_transform = mask(image, geoms, crop=True)
-            
-    #     im_tensor = clipped_image.transpose(1, 2, 0)
-    #     plt.figure(figsize = (15, 30))
-    #     plt.subplot(3, 1, 1)
-    #     plt.imshow(im_tensor)
-        
-    #     return im_tensor
-    
-
-#%%
-
-# image_dir = '/Users/thomaskeeley/Documents/School/Capstone-master/Thomas/'
-# aoi_dir = '/Users/thomaskeeley/Documents/School/Capstone-master/Thomas/test_aoi_small.geojson'
-# username = 'sirthomasnewton'
-# password = 'Coronavirus1!'
-# date_range = ('20190101', '20200101')
-        
-# instance = GetSentinelImagery(image_dir, aoi_dir, username, password, date_range)
-        
-# #%%
-
-# image = instance.clip_image()
-
-# #%%
-
-# with rasterio.Env():
-
-#     # Write an array as a raster band to a new 8-bit file. For
-#     # the new file's profile, we start with the profile of the source
-#     profile = clipped_image.profile
-
-#     # And then change the band count to 1, set the
-#     # dtype to uint8, and specify LZW compression.
-#     profile.update(
-#         dtype=rasterio.uint8,
-#         count=1,
-#         compress='lzw')
-
-#     with rasterio.open('example.tif', 'w', **profile) as dst:
-#         dst.write(clipped_image.astype(rasterio.uint8), 1)
-
-
-
-
-
-
-# im = Image.fromarray(clipped_image)
-# im.save('test.tif')
-
-# out_transform_copy = out_transform.copy()
-# with fiona.open("clipped_image.tif", "w", **out_transform) as final:
-#     final.write(clipped_image)
-
-# image.bands
-# im_bands = im.count
-        
-#         im_width = im.width
-#         im_height = im.height
-        
-#         left, bottom, right, top = im.bounds
-        
-#         im_x_res = (right-left)/im_width
-#         im_y_res = (top-bottom)/im_height  
-        
-#         crs = str(im.crs).split(':')[1] 
